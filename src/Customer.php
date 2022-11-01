b9c106c63132f37d9827770ed022d25c4d47852e
RyzzTech <102559807+Ryzz123@users.noreply.github.com>
2022-11-01 18:08:05 +0700

    Contoh Sederhana Composer

---
+++ src/Customer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Febrianandalubis\Belajar;
+
+class Customer
+{
+    public function __construct(private string $name)
+    {
+
+    }
+
+    public function sayHello(string $name): string {
+        return "Hello $name, My Name is $this->name";
+    }
+}
